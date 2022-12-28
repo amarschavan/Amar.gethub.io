8ea0156415539253ee219dce0fe0c22c125cadab
amarschavan <121539961+amarschavan@users.noreply.github.com>
2022-12-28 12:44:27 +0530

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1624591.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 116357853, 116357861, 116357863, 116357877, 116357880, 116357887 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/116357846/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/116357847/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/116357845/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/116357850/1/basis.wasm.js', 'wasmUrl' : 'files/assets/116357851/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/116357849/1/basis.js', 'preload' : false},
+];
